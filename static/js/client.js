382bb9086acd0b4e0b4656a44c4ec41b3c60bf25
ghodawalaaman <ghodawalaaman@gmail.com>
2023-07-11 12:55:11 +0530

    progress

---
+++ static/js/client.js
@@ -0,0 +1,32 @@
+const socket = io('http://localhost:5000')
+
+const form = document.getElementById('form')
+const msginp = document.getElementById('input-msg')
+const msgcontainer = document.querySelector(".container")
+
+let append = (msg,position)=>{
+    const msgelement = document.createElement('div')
+    msgelement.innerText = msg;
+    msgelement.classList.add('massage')
+    msgelement.classList.add(position)
+    msgcontainer.append(msgelement)
+}
+
+socket.emit('new-usr-joined');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const massage = msginp.value 
+    append("you:"+massage,'right')
+    socket.emit('send',massage)
+    msginp.value = ''
+})
+// const name = prompt("enter your name");
+
+
+socket.on('user-joind',name=>{
+    append('userjoind '+name,'center')
+})
+socket.on('receive',data=>{
+    append(data.name+":"+data.massage,'left')
+})
